2f2e4c620ee78e8dc7508119973d0facc5292107
Nimmagadda Vineesh <nsrvineesh@gmail.com>
2023-06-13 09:06:59 +0800

    designed car and put it on the map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,61 +1,117 @@
-const obstacles = require('./obstacles.js').obstacles;
+import React from 'react';
+import CarIcon from './CarIcon';
+const obstacles = require('./obstacles').obstacles;
 
 const gridSize = 500;
 const gridCount = 50; // No. of squares in each direction
-
+const squareSize = gridSize / gridCount;
 const coordsToObstacles = [];
+
 obstacles.forEach(([xStart, xEnd, yStart, yEnd, color]) => {
   let x = xStart;
   while (x <= xEnd) {
     let y = yStart;
     while (y <= yEnd) {
-      coordsToObstacles[`${x}:${y}`] = color || '#d77a61';
+      coordsToObstacles[`${x}:${y}`] = color || '#c1c3c7';
       y += 1;
     }
     x += 1;
   }
 });
 
-const SVG = () => {
-  const squareSize = gridSize / gridCount;
-  const Obstacle = ({ type, x, y, color }) => (
-    <rect
-      width={squareSize}
-      height={squareSize}
-      x={x}
-      y={y}
-      fill={color}
-      stroke={color}
-    />
-  );
+const Obstacle = ({ x, y, color }) => (
+  <rect
+    width={squareSize}
+    height={squareSize}
+    x={x}
+    y={y}
+    fill={color}
+    stroke={color}
+  />
+);
 
-  const obstacleElems = [];
-  for (let [key, color] of Object.entries(coordsToObstacles)) {
-    const [x, y] = key.split(':');
-    obstacleElems.push(
-      <Obstacle
-        key={`${x}:${y}`}
-        x={x * squareSize}
-        y={y * squareSize}
-        color={color}
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      position: props.next,
+      rotation: props.rotation,
+    };
+  }
+
+  render() {
+    const { position, rotation } = this.state;
+    const [x, y] = position;
+    return (
+      <CarIcon
+        x={x * squareSize - 20}
+        y={y * squareSize - 20}
+        rotation={rotation}
       />
     );
   }
+}
 
-  return (
-    <svg
-      width={gridSize}
-      height={gridSize}
-    >
-      {obstacleElems}
-    </svg>
-  )
-};
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cars: [],
+    };
+  }
+
+  async placeCars() {
+    this.setState(() => ({
+      cars: [
+        {
+          id: 'car1',
+          next: [41,39],
+          rotation: 0,
+        },
+        {
+          id: 'car2',
+          next: [51,41],
+          rotation: 270,
+        }
+      ]
+    }));
+  }
+
+  componentDidMount() {
+    this.placeCars();
+  }
+
+  render() {
+    const obstacleElems = [];
+    for (let [key, color] of Object.entries(coordsToObstacles)) {
+      const [x, y] = key.split(':');
+      obstacleElems.push(
+        <Obstacle
+          key={`${x}:${y}`}
+          x={x * squareSize}
+          y={y * squareSize}
+          color={color}
+        />
+      );
+    }
+
+    const cars = this.state.cars.map(({ id, next, rotation }) => {
+      return <Car key={id} next={next} rotation={rotation} />;
+    });
+    console.log(cars);
+    return (
+      <svg width={gridSize} height={gridSize} className="map">
+        {obstacleElems}
+        {cars}
+      </svg>
+    );
+  }
+}
 
 function App() {
   return (
     <div className='App'>
-      <SVG />
+      <Map />
     </div>
   );
 }
