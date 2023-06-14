57ab99908833be99f00f367b47ff7a82b4205490
Nimmagadda Vineesh <nsrvineesh@gmail.com>
2023-06-14 09:58:52 +0800

    added mechanism to move car

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import CarIcon from './CarIcon';
-const obstacles = require('./obstacles').obstacles;
+import { advanceCoord, getNextCoordIndex } from './movement';
+import { wait } from './utils';
+import records from './records';
 
+const obstacles =  require('./obstacles').obstacles;
 const gridSize = 500;
 const gridCount = 50; // No. of squares in each direction
 const squareSize = gridSize / gridCount;
-const coordsToObstacles = [];
 
+const fetchInterval = 1000;
+const refreshInterval = 33;
+
+const coordsToObstacles = [];
 obstacles.forEach(([xStart, xEnd, yStart, yEnd, color]) => {
   let x = xStart;
   while (x <= xEnd) {
@@ -19,28 +25,60 @@ obstacles.forEach(([xStart, xEnd, yStart, yEnd, color]) => {
   }
 });
 
-const Obstacle = ({ x, y, color }) => (
-  <rect
-    width={squareSize}
-    height={squareSize}
-    x={x}
-    y={y}
-    fill={color}
-    stroke={color}
-  />
-);
-
 class Car extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       position: props.next,
       rotation: props.rotation,
+      path: props.path,
     };
+  }
+
+  async move(next) {
+    const { path, position } = this.state;
+    let [currX, currY] = position;
+  
+    const startIndex = getNextCoordIndex(currX, currY, path);
+    const endIndex = path.findIndex(([x, y]) => {
+      return x === next[0] && y === next[1];
+    });
+    const section = path.slice(startIndex, endIndex + 1);
+
+    const distance = endIndex - startIndex + Math.max(currX % 1, currY % 1);
+    const steps = fetchInterval / refreshInterval;
+    const increment = distance / steps;
+  
+    for (let i = 0; i < section.length; i++) {
+
+      const [nextX, nextY] = section[i];
+
+      while (currX !== nextX) {
+        if (next !== this.props.next) return;
+
+        currX = advanceCoord(currX, nextX, increment);
+        this.setState({ position: [currX, this.state.position[1]] });
+        await wait(refreshInterval);
+      }
+
+      while (currY !== nextY) {
+        if (next !== this.props.next) return;
+
+        currY = advanceCoord(currY, nextY, increment);
+        this.setState({ position: [this.state.position[0], currY] });
+        await wait(refreshInterval);
+      }
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.next === this.props.next) return;
+    this.move(this.props.next);
   }
 
   render() {
     const { position, rotation } = this.state;
+
     const [x, y] = position;
     return (
       <CarIcon
@@ -60,25 +98,15 @@ class Map extends React.Component {
     };
   }
 
-  async placeCars() {
-    this.setState(() => ({
-      cars: [
-        {
-          id: 'car1',
-          next: [41,39],
-          rotation: 0,
-        },
-        {
-          id: 'car2',
-          next: [51,41],
-          rotation: 270,
-        }
-      ]
-    }));
+  async simulate() {
+    for (const record of records) {
+      this.setState({ cars: [record] });
+      await wait(fetchInterval);
+    }
   }
 
   componentDidMount() {
-    this.placeCars();
+    this.simulate();
   }
 
   render() {
@@ -86,21 +114,29 @@ class Map extends React.Component {
     for (let [key, color] of Object.entries(coordsToObstacles)) {
       const [x, y] = key.split(':');
       obstacleElems.push(
-        <Obstacle
+        <rect
           key={`${x}:${y}`}
+          width={squareSize}
+          height={squareSize}
           x={x * squareSize}
           y={y * squareSize}
-          color={color}
+          fill={color}
+          stroke={color}
         />
       );
     }
 
-    const cars = this.state.cars.map(({ id, next, rotation }) => {
-      return <Car key={id} next={next} rotation={rotation} />;
+    const cars = this.state.cars.map(({ id, next, rotation, path }) => {
+      // console.log(path)
+      return <Car key={id} next={next} rotation={rotation || 0} path={path} />;
     });
-    console.log(cars);
+
     return (
-      <svg width={gridSize} height={gridSize} className="map">
+      <svg
+      width={gridSize}
+      height={gridSize}
+      className="map"
+      >
         {obstacleElems}
         {cars}
       </svg>
